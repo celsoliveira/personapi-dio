6a800df00836f3596c1a9c0836f09a3c33307137
Celso de Oliveira Silva <celsoliveira@gmail.com>
2021-04-23 07:16:43 -0300

    Rest API With Spring Boot - Including file

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook(){
+        return "API Test!";
+    }
+
+}

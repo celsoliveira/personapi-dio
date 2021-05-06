c551308d2db5a19b26978d6a6017a72da6f02f98
Celso de Oliveira Silva <celsoliveira@gmail.com>
2021-05-06 19:48:21 -0300

    Developed list all method

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -8,6 +8,9 @@ import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
@@ -29,5 +32,12 @@ public class PersonService {
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> appPeople = personRepository.findAll();
+        return appPeople.stream()           //conversão em uma lista de objetos PersonDTO
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
